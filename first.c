c7f14424689cb8c3413278f6b30f2c13f3951d65
agathachatter <agatha.chatterjee@cumminscollege.in>
2016-08-12 15:59:58 +0530

    Update first.c

--- first.c
+++ first.c
@@ -4,5 +4,11 @@ void main()
   int a,b,c;
   println("Enter two numbers for calculation");
   scanf("%d %d",a,b);
-
+  c=a+b;
+  println("the sum is %d",c);
+  
+  int sub(a,b)
+  {
+    return a-b;
+  }
 }
